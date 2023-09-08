9ea3d42a1c29ebd5d7d2df7482841bbd9628b10a
Sumat Mallick <115460605+SumatM@users.noreply.github.com>
2023-09-08 19:34:28 +0530

    logout endpoint changed

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { connection } = require("./db");
 const { userRouter } = require("./routes/users.routes");
 const { courseRoute } = require("./routes/courses.route");
 const { videoRoute } = require("./routes/videos.route");
+
 const cors = require('cors')
+
 
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
@@ -11,6 +13,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(cors())
+
 
 app.use(express.json());
 
@@ -37,6 +40,14 @@ app.get("/regenerateToken", (req, res) => {
     res.status(400).json({ msg: "not a valid Refresh Token" });
   }
 });
+
+app.get('/',(req,res)=>{
+  try{
+    res.status(200).json({message:"Welcome to Arivu's Backend"})
+  }catch(err){
+    res.status(400).json({ message: "Some Error Occur. Please Refresh" });
+  }
+})
 
 app.listen(process.env.port, async () => {
   try {
